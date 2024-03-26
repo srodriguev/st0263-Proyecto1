017ddab3e6bcd101111e4fbc88c23daf2d22193a
srodriguev <srodriguev@eafit.edu.co>
2024-03-26 11:32:31 -0500

    client work

--- Client/Client.py
+++ Client/Client.py
@@ -4,6 +4,9 @@ from flask import Flask, request, jsonify
 import json
 import configparser
 import bcrypt
+import os
+
+from Helpers.fileSplitter import split_file_into_blocks 
 
 import socket
 
@@ -14,6 +17,15 @@ import grpc
 app = Flask(__name__)
 
 
+def testSplit(output_directory):
+    # Asegúrate de que el directorio de salida exista
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # Llama a la función split_file_into_blocks con el archivo de entrada y el directorio de salida
+    split_file_into_blocks(os.path.join(local_files, "LoremIpsum.txt"), output_directory)
+
+
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -22,4 +34,11 @@ if __name__ == '__main__':
     ip = config['Client']['ip']
     port = config['Client']['port']
     local_files = config['Client']['local_files']
+    block_output = config['Client']['block_output']
+
+
+    testSplit(block_output)
+
     app.run(host=ip, debug=True, port=int(port))
+
+
